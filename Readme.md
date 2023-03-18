d726ef2d1b88f3c3bc08aa0d4a43447c93da4e89
h2plucifer <haribp65@gmail.com>
2023-03-19 00:04:55 +0530

    Nodejs-Reactjs microservice based application

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Simple Microservice based node.js application
+Microservices :
+
+- posts // for creating & listing posts
+- comments // for creating & listing comments
+- query // for providing post & comment data
+- event-bus // acts as event service for handling different events from different services
+- moderation // for providing comment review before it can be displayed in UI
+
+client : React js app to create posts & comments
---
+++ client/src/PostList.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4002/posts");
+
+    setPosts(res.data);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  const postData = Object.values(posts);
+  const renderPosts = postData.map((post) => {
+    return (
+      <div
+        className="card"
+        key={post.id}
+        style={{ width: "30%", marginBottom: "20px" }}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          {/* <CommentList postId={post.id} /> */}
+          <CommentList comments={post.comments} />
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderPosts}
+    </div>
+  );
+};
+
+export default PostList;
